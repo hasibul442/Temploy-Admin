dab832a1977d4e6786598f51db10a836ab8b4553
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-10-24 21:31:59 +0600

    feat: Remove unused components and clean up project structure

--- src/app/(admin)/(settings)/categories/page.jsx
+++ src/app/(admin)/(settings)/categories/page.jsx
@@ -1,1 +1,24 @@
-rfce
+"use client";
+import React, { useState } from 'react'
+import { GetRequestData, PostRequestData } from "@/Helper/HttpRequestHelper";
+
+function Page() {
+    const [categories, setCategories] = useState([])
+
+  const fetchCategories = () => {
+    GetRequestData(`api/v1/categories?page=${currentPage}&limit=10&search=${searchTerm}`).then((data) => {
+      setCategories(data);
+    });
+  };
+  return (
+    <>
+        <div className="row">
+            <div className="col">
+                <h1>Categories</h1>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Page
