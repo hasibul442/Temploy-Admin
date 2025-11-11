58d1b1f057700c6537ada824464e9d014b2ce86a
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-11-11 15:43:51 +0600

    Add ToastContainer and update layout structure

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 
+import { Bounce, ToastContainer } from "react-toastify";
 import "./globals.css";
 import Script from 'next/script';
 
@@ -15,17 +16,44 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning={true}>
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/assets/img/apple-icon.png" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/assets/img/apple-icon.png"
+        />
         <link rel="icon" type="image/png" href="/assets/img/favicon.png" />
-        <link href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,800" rel="stylesheet" />
-        <link href="https://demos.creative-tim.com/soft-ui-dashboard/assets/css/nucleo-icons.css" rel="stylesheet" />
-        <link href="https://demos.creative-tim.com/soft-ui-dashboard/assets/css/nucleo-svg.css" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,800"
+          rel="stylesheet"
+        />
+        <link
+          href="https://demos.creative-tim.com/soft-ui-dashboard/assets/css/nucleo-icons.css"
+          rel="stylesheet"
+        />
+        <link
+          href="https://demos.creative-tim.com/soft-ui-dashboard/assets/css/nucleo-svg.css"
+          rel="stylesheet"
+        />
         {/* <script src="https://kit.fontawesome.com/42d5adcbca.js" crossOrigin="anonymous"></script> */}
-        <link id="pagestyle" href="/assets/css/soft-ui-dashboard.css?v=1.1.0" rel="stylesheet" />
+        <link
+          id="pagestyle"
+          href="/assets/css/soft-ui-dashboard.css?v=1.1.0"
+          rel="stylesheet"
+        />
         {/* <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script> */}
       </head>
       <body suppressHydrationWarning={true}>
+        <ToastContainer
+          theme="dark"
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          transition={Bounce}
+        />
 
         {children}
 
@@ -41,8 +69,16 @@ export default function RootLayout({ children }) {
               Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
             }`}
         </Script> */}
-        <Script src="https://buttons.github.io/buttons.js" strategy="lazyOnload" async defer />
-        <Script src="/assets/js/soft-ui-dashboard.min.js?v=1.1.0" strategy="lazyOnload" />
+        <Script
+          src="https://buttons.github.io/buttons.js"
+          strategy="lazyOnload"
+          async
+          defer
+        />
+        <Script
+          src="/assets/js/soft-ui-dashboard.min.js?v=1.1.0"
+          strategy="lazyOnload"
+        />
       </body>
     </html>
   );
